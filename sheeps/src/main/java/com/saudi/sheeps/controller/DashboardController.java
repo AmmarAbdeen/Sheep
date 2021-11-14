1074881ef3793364a5d5d57d5fcbe5783e256843
AmmarAbdeen <abdeenammar14@gmail.com>
2021-11-14 21:10:17 +0200

    npm i

---
+++ sheeps/src/main/java/com/saudi/sheeps/controller/DashboardController.java
@@ -0,0 +1,62 @@
+package com.saudi.sheeps.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saudi.sheeps.service.DashbordService;
+import com.saudi.sheeps.service.JWTService;
+
+import io.jsonwebtoken.Claims;
+import lombok.extern.apachecommons.CommonsLog;
+
+@RestController
+@CommonsLog
+@RequestMapping("/api/sheep/dashboard")
+public class DashboardController extends BaseController {
+	@Autowired
+	private DashbordService dashbordService;
+	@Autowired
+	protected JWTService jwtservice;
+	
+	
+	@GetMapping(value = "/getallsheepgroupbytype")
+	public ResponseEntity<?> getAllSheepGroupByType(@RequestHeader("session-token") String sessionToken) {
+		try {
+			Claims tokenInfo = jwtservice.decodeJWT(sessionToken).getBody();
+			return success(dashbordService.getAllSheepGroupByType());
+
+		} catch (Exception e) {
+			return wrapException(e, "getAllSheepGroupByType");
+
+		}
+	}
+	
+	@GetMapping(value = "/getalllambsgroupbytype")
+	public ResponseEntity<?> getAllLambsGroupByType(@RequestHeader("session-token") String sessionToken) {
+		try {
+			Claims tokenInfo = jwtservice.decodeJWT(sessionToken).getBody();
+			return success(dashbordService.getAllLambsGroupByType());
+
+		} catch (Exception e) {
+			return wrapException(e, "getAllLambsGroupByType");
+
+		}
+	}
+	
+	@GetMapping(value = "/getallamountofstoredfeed")
+	public ResponseEntity<?> getAllAmountOfStoredFeed(@RequestHeader("session-token") String sessionToken) {
+		try {
+			Claims tokenInfo = jwtservice.decodeJWT(sessionToken).getBody();
+			return success(dashbordService.getAllAmountOfStoredFeed());
+
+		} catch (Exception e) {
+			return wrapException(e, "getAllAmountOfStoredFeed");
+
+		}
+	}
+
+}
--- sheepsFrontend/src/app/pages/dashboard.component.ts
+++ sheepsFrontend/src/app/pages/dashboard.component.ts
@@ -5,84 +5,80 @@ import {MenuItem} from 'primeng/primeng';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { Subscription } from 'rxjs';
+import {ChartModule} from 'primeng/chart';
 
 @Component({
     templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
 
-    cities: SelectItem[];
+    data: any;
 
-    cols: any[];
+    subscription: Subscription;
 
-    chartData: any;
-
-    events: any[];
-
-    selectedCity: any;
-
-
-    items: MenuItem[];
-
-    fullcalendarOptions: any;
+    employeeOrgData; orgCount1;
+    orgCount2; orgCount3; orgCount4; orgCount5; employeeLabel: any;
+    employeeJIRAHoursData;
+  
+    
+    employeeOrganizationData = [
+      { name: 'employee1', designation: 'manager', employer: 'organization1' },
+      { name: 'employee2', designation: 'manager', employer: 'organization1' },
+      { name: 'employee3', designation: 'manager', employer: 'organization1' },
+      { name: 'employee4', designation: 'manager', employer: 'organization1' },
+      { name: 'employee5', designation: 'manager', employer: 'organization2' },
+      { name: 'employee6', designation: 'manager', employer: 'organization2' },
+      { name: 'employee7', designation: 'manager', employer: 'organization2' },
+      { name: 'employee8', designation: 'manager', employer: 'organization3' },
+      { name: 'employee9', designation: 'manager', employer: 'organization3' },
+      { name: 'employee10', designation: 'manager', employer: 'organization3' },
+      { name: 'employee11', designation: 'manager', employer: 'organization3' },
+      { name: 'employee12', designation: 'manager', employer: 'organization3' },
+      { name: 'employee13', designation: 'manager', employer: 'organization3' },
+      { name: 'employee14', designation: 'manager', employer: 'organization3' },
+      { name: 'employee15', designation: 'manager', employer: 'organization4' },
+      { name: 'employee16', designation: 'manager', employer: 'organization4' },
+      { name: 'employee17', designation: 'manager', employer: 'organization5' },
+      { name: 'employee18', designation: 'manager', employer: 'organization5' },
+      { name: 'employee19', designation: 'manager', employer: 'organization5' },
+      { name: 'employee20', designation: 'manager', employer: 'organization5' },
+      { name: 'employee21', designation: 'manager', employer: 'organization5' }
+    ]
 
     constructor( private breadcrumbService: BreadcrumbService) {
-        this.breadcrumbService.setItems([
-            {label: ''}
-        ]);
+       
     }
 
     ngOnInit() {
-      
-        this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
-        ];
 
-        
+        this.orgCount1 = this.employeeOrganizationData.filter(emp => emp.employer === 'organization1').length;
+        this.orgCount2 = this.employeeOrganizationData.filter(emp => emp.employer === 'organization2').length;
+        this.orgCount3 = this.employeeOrganizationData.filter(emp => emp.employer === 'organization3').length;
+        this.orgCount4 = this.employeeOrganizationData.filter(emp => emp.employer === 'organization4').length;
+        this.orgCount5 = this.employeeOrganizationData.filter(emp => emp.employer === 'organization5').length;
+    
+        this.employeeLabel =
+    
+          this.employeeOrganizationData.map(emp => emp.employer)
+            .filter((value, index, self) => self.indexOf(value) === index);
 
-        this.cities = [];
-        this.cities.push({label: 'Select City', value: null});
-        this.cities.push({label: 'New York', value: {id: 1, name: 'New York', code: 'NY'}});
-        this.cities.push({label: 'Rome', value: {id: 2, name: 'Rome', code: 'RM'}});
-        this.cities.push({label: 'London', value: {id: 3, name: 'London', code: 'LDN'}});
-        this.cities.push({label: 'Istanbul', value: {id: 4, name: 'Istanbul', code: 'IST'}});
-        this.cities.push({label: 'Paris', value: {id: 5, name: 'Paris', code: 'PRS'}});
-
-        this.chartData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            
+        this.data = {
+            labels: this.employeeLabel,
             datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    borderColor: '#FFC107'
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    borderColor: '#03A9F4'
-                }
+              {
+                data: [this.orgCount1, this.orgCount2, this.orgCount3, this.orgCount4, this.orgCount5],
+                backgroundColor: ['#ff0000', '#0000FF', '#FFFF00', '#FFC0CB', '#7f00ff ']
+              }
             ]
-        };
+          }
 
-        this.items = [
-            {label: 'Save', icon: 'ui-icon-check'},
-            {label: 'Update', icon: 'ui-icon-refresh'},
-            {label: 'Delete', icon: 'ui-icon-delete'}
-        ];
 
-        this.fullcalendarOptions = {
-            plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
-            defaultDate: '2016-01-12',
-            header: {
-                left: 'prev,next, today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }
-        };
+       
     }
+
+   
+
+   
 }
